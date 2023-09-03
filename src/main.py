afb06cd23ce47e3235cfb00c6aa47e0899fbdffe
Prabhu <basavaprabhu.ukuk@gmail.com>
2023-09-03 20:47:04 +0100

    list comprehension and slicing

--- src/main.py
+++ src/main.py
@@ -1,22 +1,24 @@
 todos = []
  
-to_do_file ="../data/todos.txt"
+to_do_file ="./data/todos.txt"
 while True:
     user_input= input("Type add[1], edit[2], show[3] , complete[4], exit[x]:")
     user_input =user_input.strip()
 
-    if user_input=="1":
+    if "1" in user_input:
         file1 = open(to_do_file, 'r')
         todos = file1.readlines() 
         file1.close()
         
-        new_item = input("Enter a to-do:") + '\n'
+        #new_item = input("Enter a to-do:") + '\n'
         
+        new_item  = user_input[2:] +'\n'
+
         file_w = open(to_do_file, 'w')
         todos.append(new_item)
         file_w.writelines(todos)
         file_w.close()
-    elif user_input =="2":
+    elif "2" in user_input:
         item = int(input("Enter a place to replace the todo(Int):"))
         item = item-1
         with open(to_do_file, 'a+' ) as fp_edit:
@@ -27,7 +29,7 @@ while True:
         with open(to_do_file, 'w' ) as fp_edit:
             fp_edit.writelines(todos)
     
-    elif user_input =="3":
+    elif "3" in user_input:
         with open(to_do_file, 'r') as fp_r:
            display=fp_r.readlines()
         
@@ -36,7 +38,7 @@ while True:
         for index, to_do_item in enumerate(new_to_do):
             print (f'{index+1}-->{to_do_item}')
     
-    elif user_input =="4":
+    elif "4" in user_input:
         complete = int(input("Enter which item to be completed!:"))
         complete = complete-1
         
@@ -51,7 +53,10 @@ while True:
 
         print(f"The to-do item removed was : '{to_do_to_remove}'")
 
-    elif user_input =="x":
+    elif "x" in user_input:
         break
+
+    else:
+        print("Invalid entry!!!!")
 
 print("Bye!!!!!!")
